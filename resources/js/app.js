1c1ee83aaba51654a051515e32973608f5fdc6da
Paul McBride <hello@paulmcbride.net>
2017-11-14 20:28:04 +0000

    feature: add progressive image enhancement to homepage

--- resources/js/app.js
+++ resources/js/app.js
@@ -82,4 +82,27 @@ $(document).ready(function() {
 
   window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
   heap.load(php_vars.heap_key);
+
+  $('.c-post-card__image').each((index, item) => {
+    // console.log($(item).find('.c-post-card__image__sharp').data('image'));
+    var sharpDiv = $(item).find('.c-post-card__image__sharp');
+    var smallDiv = $(item).find('.c-post-card__image__small');
+    var imageUrl = sharpDiv.data('image');
+    swapBg(imageUrl, sharpDiv, smallDiv);
+  })
 });
+
+
+function swapBg(imageSrc, sharpDiv, originalDiv)
+{
+  $('<img/>').attr('src', imageSrc).on('load', function()
+  {
+    $(sharpDiv).css('background-image', 'url('+imageSrc+')');
+
+    $(originalDiv).fadeOut(500, function() {
+      $(this).remove();
+    });
+
+    $(this).remove();
+  });
+}
