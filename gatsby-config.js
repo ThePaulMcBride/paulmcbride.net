5012be32cf174e8833c9c476f12243b2c7470453
Paul McBride <hello@paulmcbride.net>
2018-10-04 14:10:53 +0100

    install gatsby-plugin-markdown-pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,6 +61,7 @@ module.exports = {
         ],
       },
     },
+    'gatsby-plugin-markdown-pages',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
---
+++ src/pages/about-us.md
@@ -0,0 +1,6 @@
+---
+title: About Us
+template: page
+---
+
+About us copy...
