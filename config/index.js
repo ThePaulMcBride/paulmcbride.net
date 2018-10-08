6d93a3e3186ea6b795f28f9237d6a9b3b334bd8c
Paul McBride <hello@paulmcbride.net>
2018-10-08 12:04:57 +0100

    write description for website

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'Paul McBride | Mentor, Developer and Tech Enthusiast',
   tagline: 'Mentor, Developer and Tech Enthusiast',
   siteUrl: 'https://paulmcbride.net',
-  description: 'This is placeholder description',
+  description: 'I am Paul McBride. I help other people learn to code and I build things for the web. I currently live in Belfast, Northern Ireland.',
   url: 'https://paulmcbride.net',
   logo: 'https://paulmcbride.net/logo.png',
   twitter: '@ThePaulMcBride',
