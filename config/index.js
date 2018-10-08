e42c870631648053b9971ae7c88ee641207e60f3
Paul McBride <hello@paulmcbride.net>
2018-10-08 12:13:12 +0100

    add fallback background image

--- config/index.js
+++ config/index.js
@@ -5,6 +5,7 @@ module.exports = {
   description: 'I am Paul McBride. I help other people learn to code and I build things for the web. I currently live in Belfast, Northern Ireland.',
   url: 'https://paulmcbride.net',
   logo: 'https://paulmcbride.net/logo.png',
+  image: 'https://paulmcbride.net/background.jpg',
   twitter: '@ThePaulMcBride',
   fbAppID: '128561761086914',
 };
