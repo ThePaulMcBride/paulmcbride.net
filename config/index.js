b1fdb5134189766dce0ba833d5be562dd9ac7914
Paul McBride <hello@paulmcbride.net>
2018-10-08 12:26:37 +0100

    set site url as an env var

--- config/index.js
+++ config/index.js
@@ -3,9 +3,9 @@ module.exports = {
   tagline: 'Mentor, Developer and Tech Enthusiast',
   siteUrl: 'https://paulmcbride.net',
   description: 'I am Paul McBride. I help other people learn to code and I build things for the web. I currently live in Belfast, Northern Ireland.',
-  url: 'https://paulmcbride.net',
-  logo: 'https://paulmcbride.net/logo.png',
-  image: 'https://paulmcbride.net/background.jpg',
+  url: process.env.URL || 'https://paulmcbride.net',
+  logo: process.env.URL ? `${process.env.URL}/logo.png` : 'https://paulmcbride.net/logo.png',
+  image: process.env.URL ? `${process.env.URL}/background.jpg` : 'https://paulmcbride.net/background.jpg',
   twitter: '@ThePaulMcBride',
   fbAppID: '128561761086914',
 };
