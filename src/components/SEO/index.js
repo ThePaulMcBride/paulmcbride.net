795e9a73481118693249b9327fb5536e6e82f72a
Paul McBride <hello@paulmcbride.net>
2020-05-14 11:32:34 +0100

    fix default og image path

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -37,7 +37,9 @@ const SEO = ({ postData, frontmatter = {}, postImage, isBlogPost }) => (
         frontmatter || postData.childMarkdownRemark.frontmatter || {}
       const title = isBlogPost ? postMeta.title : config.siteTitle
       const description = postMeta.description || seo.description
-      const image = postImage ? `${seo.canonicalUrl}${postImage}` : seo.image
+      const image = postImage
+        ? `${seo.canonicalUrl}${postImage}`
+        : `${seo.canonicalUrl}${path.sep}${seo.image}`
       const url = postMeta.slug
         ? `${seo.canonicalUrl}${path.sep}${postMeta.slug}`
         : seo.canonicalUrl
