4a18bbef27168cb8a28a4cde820baa48d38fb39b
Paul McBride <hello@paulmcbride.net>
2020-06-17 17:52:48 +0100

    instal bundle analyizer

--- gatsby-config.js
+++ gatsby-config.js
@@ -153,5 +153,11 @@ module.exports = {
         exclude: [`/messages`],
       },
     },
+    {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        devMode: true,
+      },
+    },
   ],
 }
